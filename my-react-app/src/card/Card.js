7ea92e3e028f5bbb0cafe423d2ae675f0e7529fd
tgd <zjrzpp0414@163.com>
2018-06-14 14:18:38 +0800

    style属性和dangerouslySetInnerHTML

--- my-react-app/src/card/Card.js
+++ my-react-app/src/card/Card.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
+    constructor() {
+        super()
+        this.state = {
+            content: '<div>哈哈</div>',
+            color: 'red'
+        }
+    }
     render() {
         console.log(this.props.children)
         return (
@@ -8,6 +15,14 @@ class Card extends Component {
                 <div className="card-content">
                     {this.props.children}
                 </div>
+                {/* 考虑到XSS攻击，被自动转义 */}
+                <p>{this.state.content}</p>
+
+                {/* 这个属性不必要的情况就不要使用 */}
+                <p  dangerouslySetInnerHTML={{__html: this.state.content}}></p>
+
+                {/* style属性的使用： 变成一个对象再传给元素 */}
+                <h1 style={{fontSize: '12px', color: this.state.color}}>React.js 小书</h1>
             </div>
         );
     }
