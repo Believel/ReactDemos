c43e46cc66fa3a398e4940dd29cc31bc771df018
tgd <zjrzpp0414@163.com>
2018-06-22 16:35:52 +0800

    主题颜色切换

--- make-redux/src/App.js
+++ make-redux/src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import PropTypes from 'prop-types';
+import Header from './view/Header';
+import Content from './view/Content';
+function createStore (reducer) {
+  let state = null;
+  const listeners = [];
+  const subscribe = (listener) => listeners.push(listener);
+  const getState = () => state;
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener())
+  }
+  dispatch({}) // 初始化state
+  return {getState, dispatch, subscribe}
+}
+// 定义一个表示主题色的状态
+const themeReducer = (state, action) => {
+  if (!state) return {
+    themeColor: 'red'
+  }
+  switch (action.type) {
+    case 'CHANGE_COLOR': 
+    return {...state, themeColor: action.themeColor}
+    default:
+    return state
+  }
+}
+const store = createStore(themeReducer)
 class App extends Component {
+  static childContextTypes = {
+    store: PropTypes.object
+  }
+  getChildContext () {
+    return {store}
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header/>
+        <Content/>
       </div>
     );
   }
